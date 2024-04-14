7deb5789b766ee4eaacad7684906bacd9456cba1
jasonchen <j267chen@uwaterloo.ca>
2024-04-14 15:32:19 -0400

    docker update

--- frontend-next/src/app/layout.jsx
+++ frontend-next/src/app/layout.jsx
@@ -5,9 +5,8 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}) 
+{
   return (
     <html lang="en">
       <body>{children}</body>
